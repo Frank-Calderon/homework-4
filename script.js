2bfcc07080ac44b429a36468218f23f906bb189a
Frank <fcalderon3291@gmail.com>
2020-03-01 15:00:10 -0500

    Added html page and question java script code

---
+++ script.js
@@ -0,0 +1,120 @@
+// Global Variables
+var score = 0;
+
+// DOM elements
+var totalScore = document.getElementById("score");
+
+
+// Quiz questions
+var quizQuestions = [{
+    question: "Inside which HTML element do we put the JavaScript?",
+    options: [
+        "<scripting>",
+        "<javascript>",
+        "<script>",
+        "<js>"
+    ],
+    answer = function () {
+       return this.options[2]
+    },
+
+    question = "Question",
+    options: [
+        "option 1",
+        "option 2",
+        "option 3",
+        "option 4"
+    ],
+    answer = function () {
+       return this.options[3]
+    }, 
+
+    question = "Question",
+    options: [
+        "option 1",
+        "option 2",
+        "option 3",
+        "option 4"
+    ],
+    answer = function () {
+       return this.options[3]
+    }, 
+
+    question = "Question",
+    options: [
+        "option 1",
+        "option 2",
+        "option 3",
+        "option 4"
+    ],
+    answer = function () {
+       return this.options[3]
+    }, 
+
+    question = "Question",
+    options: [
+        "option 1",
+        "option 2",
+        "option 3",
+        "option 4"
+    ],
+    answer = function () {
+       return this.options[3]
+    }, 
+
+    question = "Question",
+    options: [
+        "option 1",
+        "option 2",
+        "option 3",
+        "option 4"
+    ],
+    answer = function () {
+       return this.options[3]
+    }, 
+
+    question = "Question",
+    options: [
+        "option 1",
+        "option 2",
+        "option 3",
+        "option 4"
+    ],
+    answer = function () {
+       return this.options[3]
+    }, 
+
+    question = "Question",
+    options: [
+        "option 1",
+        "option 2",
+        "option 3",
+        "option 4"
+    ],
+    answer = function () {
+       return this.options[3]
+    }, 
+
+    question = "Question",
+    options: [
+        "option 1",
+        "option 2",
+        "option 3",
+        "option 4"
+    ],
+    answer = function () {
+       return this.options[3]
+    }, 
+
+    question = "Question",
+    options: [
+        "option 1",
+        "option 2",
+        "option 3",
+        "option 4"
+    ],
+    answer = function () {
+       return this.options[3]
+    }, 
+}]
+
